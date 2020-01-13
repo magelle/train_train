127cf16b1824c4df5d002a8586fb57d2fe605732
Maxime GELLE <maxime.gelle@gmail.com>
2020-01-13 19:27:39 +0100

    Add test coverage config

--- mix.exs
+++ mix.exs
@@ -28,7 +28,9 @@ defmodule TrainTrain.Umbrella.MixProject do
   # Dependencies listed here are available only for this project
   # and cannot be accessed from applications inside the apps folder
   defp deps do
-    []
+    [
+      {:excoveralls, "~> 0.10", only: :test}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
