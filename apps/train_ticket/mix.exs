e432a1e0b31f90f66e6703dc4d4167b2fbddd81a
Maxime GELLE <maxime.gelle@gmail.com>
2020-01-13 19:04:04 +0100

    Add test coverage config

--- apps/train_ticket/mix.exs
+++ apps/train_ticket/mix.exs
@@ -18,9 +18,7 @@ defmodule TrainTicket.MixProject do
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.travis": :test
       ]
     ]
   end
--- mix.exs
+++ mix.exs
@@ -10,9 +10,7 @@ defmodule TrainTrain.Umbrella.MixProject do
       test_coverage: [tool: ExCoveralls],
       preferred_cli_env: [
         coveralls: :test,
-        "coveralls.detail": :test,
-        "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.travis": :test
       ]
     ]
   end
